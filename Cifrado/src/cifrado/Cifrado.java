3e72a13ec6fbd44e81e4c6d77e504a8eeeba0cfb
ISRAEL <ISRAEL@ISRAEL>
2016-06-14 18:07:31 -0500

    Primer Commit

---
+++ Cifrado/src/cifrado/Cifrado.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cifrado;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ISRAEL
+ */
+public class Cifrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] arg = {1, 2, 3, 5, 6};
+
+        System.out.println(Arrays.binarySearch(args, 2));
+
+    }
+
+}
